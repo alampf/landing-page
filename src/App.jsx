803718f9a0626c57fec7a5e163775d550cb19f6a
Carlos Hernández <charlyc1502@gmail.com>
2026-06-08 14:25:41 -0600

    huevos

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import Contact from './pages/contact'
+import About from './components/About'
 
 function App() {
   return (
     <div className='min-h-screen overflow-x-hidden'>
       <main>
       <Contact />
+      <About />
       </main>
     </div>
   )
