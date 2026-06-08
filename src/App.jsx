bd14297f882d7b8b5292a26894a54b57fa1d372e
antony <antonydelacruzramos@gmail.com>
2026-06-08 14:36:37 -0600

    portafolio agregado

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import Contact from './pages/contact'
+import Portfolio from './pages/portafolio'
 
 function App() {
   return (
     <div className='min-h-screen overflow-x-hidden'>
       <main>
+      <Portfolio/>
       <Contact />
       </main>
     </div>
