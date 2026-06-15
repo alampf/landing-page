503b1e23abc8345371a108769d071d66e203733e
alanlopezromero <alopezromero192@gmail.com>
2026-06-15 13:44:48 -0600

    Actualiza componente About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,20 +1,24 @@
 function About() {
   return (
-    <section id="about">
-      <h2>Acerca de Nosotros</h2>
+    <section id="about" className="bg-red-500 text-white p-20">
+      <div className="max-w-4xl mx-auto px-6 text-center">
+        
+        <h2 className="text-3xl font-bold text-gray-800 mb-6">
+          Acerca de Nosotros
+        </h2>
 
-      <p>
-        Somos una empresa comprometida con ofrecer soluciones innovadoras
-        que ayuden a nuestros clientes a alcanzar sus objetivos mediante
-        tecnología y calidad de servicio.
-      </p>
+        <p className="text-gray-600 mb-4">
+          Somos una empresa comprometida con ofrecer soluciones innovadoras
+          que ayuden a nuestros clientes a alcanzar sus objetivos mediante
+          tecnología y calidad de servicio.
+        </p>
 
-      <p>
-        Nuestro equipo trabaja constantemente para brindar experiencias
-        modernas, eficientes y enfocadas en las necesidades de cada usuario.
+        <p className="text-gray-600">
+          Nuestro equipo trabaja constantemente para brindar experiencias
+          modernas, eficientes y enfocadas en las necesidades de cada usuario.
+        </p>
 
-        cara de vr
-      </p>
+      </div>
     </section>
   );
 }
